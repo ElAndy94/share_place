f5ec1f2cab1053b61c90f65fad88447124a237ad
Andrew <andrewpeliza@hotmail.com>
2020-03-16 16:02:03 +0000

    Searches your location when you input location.

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,16 @@ import { api as GOOGLE_API_KEY } from './config/api';
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+// declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
 async function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
-
-  type GoogleGeocodingResponse = {
-    results: { geometry: { location: { lat: number; lng: number } } }[];
-    status: 'OK' | 'ZERO_RESULTS';
-  };
 
   await axios
     .get<GoogleGeocodingResponse>(
@@ -24,6 +26,12 @@ async function searchAddressHandler(event: Event) {
         throw new Error('Could not fetch location!');
       }
       const cordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map')!, {
+        center: cordinates,
+        zoom: 12
+      });
+
+      new google.maps.Marker({ position: cordinates, map: map });
       console.log(cordinates);
     })
     .catch(err => {
